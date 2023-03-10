a2e6d6776a9eeed6a9bc7fe559f1b72a3fa57084
MinPan9585 <94884915+MinPan9585@users.noreply.github.com>
2023-03-10 17:48:00 +0800

    Some changes on galaxy pen 0310

--- Assets/Scripts/GalaxyPen.cs
+++ Assets/Scripts/GalaxyPen.cs
@@ -8,21 +8,93 @@ public class GalaxyPen : MonoBehaviour
     [SerializeField] private InputActionReference changePrefabAction;
     [SerializeField] private InputActionReference drawPrefabAction;
     public GameObject cloudPrefab;
-    public GameObject stonePrefab;
+    public Transform tip;
+    //float penCooldown = 1f;
+    //float penNextCooldown = 0;
+    public GameObject[] brushesPrefab;
+    int index = 0;
+    bool pressed = false;
 
     void OnEnable()
     {
         changePrefabAction.action.performed += ChangePrefab;
-        drawPrefabAction.action.performed += DrawPrefab;
+        drawPrefabAction.action.performed += setTrue;
     }
 
-    void ChangePrefab(InputAction.CallbackContext obj)
+    void setTrue(InputAction.CallbackContext ctx)
     {
-        
+        pressed = true;    }
+
+    void ChangePrefab(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            Vector2 joystickValue = ctx.ReadValue<Vector2>();
+            if (joystickValue.x >= 0.2)
+            {
+                if (index < 2)
+                {
+                    index++;
+                }
+                else
+                {
+                    index = 0;
+                }
+            }
+            if (joystickValue.x <= -0.2)
+            {
+                if (index > 0)
+                {
+                    index--;
+                }
+                else
+                {
+                    index = 2;
+                }
+            }
+        }
     }
 
-    void DrawPrefab(InputAction.CallbackContext obj)
+    void Update()
     {
-        Instantiate(cloudPrefab, transform.position, Quaternion.identity);
+        if (pressed)
+        {
+            DrawPrefab(InputAction.CallbackContext ctx);
+            Vector3 rotation = new Vector3(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f));
+            Instantiate(brushesPrefab[index], tip.position, Quaternion.Euler(rotation));
+        }
+
     }
+
+    void DrawPrefab(InputAction.CallbackContext ctx)
+    {
+        if (Time.time > penNextCooldown)
+        {
+            Instantiate(cloudPrefab, transform.position, Quaternion.identity);
+            penNextCooldown = Time.time + penCooldown;
+        }
+
+
+        if (ctx.performed)
+        {
+            pressed = true;
+            
+        }
+        if (ctx.canceled)
+        {
+            pressed = false;
+            
+        }
+    }
+
+    //IEnumerator SpawnPrefab()
+    //{
+    //    if(pressed)
+    //    {
+    //        Vector3 rotation = new Vector3(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f));
+    //        Instantiate(brushesPrefab[index], tip.position, Quaternion.Euler(rotation));
+    //        yield return new WaitForSeconds(0.4f);
+    //    }
+
+    //}
 }
